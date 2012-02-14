614c08213837b31f2675cd7d8baaa23b85bcce66
bradvin <bradvin@gmail.com>
2012-02-14 07:29:12 +0200

    fixed indenting issues

--- attachments-carousel.php
+++ attachments-carousel.php
@@ -38,80 +38,80 @@ class AttachmentsCarousel {
 	 * Initializes the plugin by setting localization, filters, and administration functions.
 	 */
 	function __construct() {
-    	// Load JavaScript and stylesheets
-      add_action( 'init', array( &$this, 'register_scripts_and_styles') );
+    		// Load JavaScript and stylesheets
+      		add_action( 'init', array( &$this, 'register_scripts_and_styles') );
 		
-      add_shortcode( 'attachments-carousel', array( &$this, 'render_carousel' ) );
+      		add_shortcode( 'attachments-carousel', array( &$this, 'render_carousel' ) );
 	} // end constructor
   
-  function render_carousel($atts) {
-    global $post;
-    global $attachments_carousel_count;
-    
-    if (empty($attachments_carousel_count)) {
-      $attachments_carousel_count = 0;
-    }
-    
-    $attachments_carousel_count++;
-    
-    $post_id = $post->ID;
-    
-    // Extract the attributes
-    extract(shortcode_atts(array(
-      'orderby' => 'rand',
-      'size' => 'medium',
-      'number' => -1,  //all
-      'target' => '_blank'
-    ), $atts));
-    
+  	function render_carousel($atts) {
+		global $post;
+		global $attachments_carousel_count;
+		
+		if (empty($attachments_carousel_count)) {
+			$attachments_carousel_count = 0;
+		}
+		
+		$attachments_carousel_count++;
+		
+		$post_id = $post->ID;
+		
+		// Extract the attributes
+		extract(shortcode_atts(array(
+			'orderby' => 'rand',
+			'size' => 'medium',
+			'number' => -1,  //all
+			'target' => '_blank'
+			), $atts));
+		
 		$html = '<!-- no attachment images found -->';
-    
-    $args = array(
-      'post_type' => 'attachment',
-      'numberposts' => $number,
-      'post_status' => null,
-      'post_parent' => $post->ID
-    );
-
-    //get all attachments for the current post
-    $attachments = get_posts( $args );
-    $i = 0;
-    
-    if ( $attachments ) {
-    
-      $html = '<div id="ca-container-' . $attachments_carousel_count . '" class="ca-container"><div class="ca-wrapper">';
-    
-      foreach ( $attachments as $attachment ) {
-        $full_img_info = wp_get_attachment_image_src( $attachment->ID, 'full' );
-        $thumb_img_info = wp_get_attachment_image_src( $attachment->ID, $size );
-        
-        $html .= '  <div class="ca-item ca-item-'.$i.'">
-    <div class="ca-item-main">
-      <a href="' . $full_img_info[0] . '" target="'.$target.'"><img src="' . $thumb_img_info[0] . '" width="' . $thumb_img_info[1] . '" height="' . $thumb_img_info[2] . '" border="0" /></a>
-    </div>
-  </div>';
-        
-        $i++;
-      }
-    }    
-        
-    $html .= '</div></div>';
-    
-    return $this->render_script($attachments_carousel_count) . $html;
-  }
+		
+		$args = array(
+			'post_type' => 'attachment',
+			'numberposts' => $number,
+			'post_status' => null,
+			'post_parent' => $post->ID
+			);
+		
+		//get all attachments for the current post
+		$attachments = get_posts( $args );
+			$i = 0;
+			
+			if ( $attachments ) {
+			
+			$html = '<div id="ca-container-' . $attachments_carousel_count . '" class="ca-container"><div class="ca-wrapper">';
+			
+			foreach ( $attachments as $attachment ) {
+				$full_img_info = wp_get_attachment_image_src( $attachment->ID, 'full' );
+				$thumb_img_info = wp_get_attachment_image_src( $attachment->ID, $size );
+				
+				$html .= '  <div class="ca-item ca-item-'.$i.'">
+				<div class="ca-item-main">
+				<a href="' . $full_img_info[0] . '" target="'.$target.'"><img src="' . $thumb_img_info[0] . '" width="' . $thumb_img_info[1] . '" height="' . $thumb_img_info[2] . '" border="0" /></a>
+				</div>
+				</div>';
+				
+				$i++;
+			}
+		}    
+		
+		$html .= '</div></div>';
+		
+		return $this->render_script($attachments_carousel_count) . $html;
+	}
   
-  function render_script($count) {
-    global $attachments_carousel_count;
-    return '<script type="text/javascript">jQuery(document).ready(function() {jQuery(\'#ca-container-'.$count.'\').contentcarousel();});</script>';
-  }
+	function render_script($count) {
+		global $attachments_carousel_count;
+		return '<script type="text/javascript">jQuery(document).ready(function() {jQuery(\'#ca-container-'.$count.'\').contentcarousel();});</script>';
+	}
 	
 	/**
 	 * Registers and enqueues stylesheets for the administration panel and the
 	 * public facing site.
 	 */
 	function register_scripts_and_styles() {
-    $this->load_file( self::slug . '-js', '/js/' . self::slug . '.js', true );
-    $this->load_file( self::slug . '-css', '/css/' . self::slug . '.css' );
+    		$this->load_file( self::slug . '-js', '/js/' . self::slug . '.js', true );
+    		$this->load_file( self::slug . '-css', '/css/' . self::slug . '.css' );
 	} // end register_scripts_and_styles
 	
 	/**
